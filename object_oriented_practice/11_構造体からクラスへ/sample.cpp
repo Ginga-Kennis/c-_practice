734c4f103cfab2a5b1901d8d0d0a6841d2c028b9
Ginga Kennis <gingakennis123@icloud.com>
2023-02-05 21:18:52 +0900

    finished_11

---
+++ object_oriented_practice/11_構造体からクラスへ/sample.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+struct Student{
+    int year;
+    int num;
+};
+
+int main(){
+    struct Student Ginga = {5,10};
+    cout << "Year:" << Ginga.year << " , Number:" << Ginga.num << "\n";
+    return 0;
+}
--- object_oriented_practice/sample.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-int main(void){
-    std::cout << "Test \n";
-    return 0;
-    
-}
