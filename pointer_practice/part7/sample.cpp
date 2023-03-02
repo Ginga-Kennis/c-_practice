063e6d3c6f0630992512b644388f0a88ace8db20
Ginga Kennis <gingakennis123@icloud.com>
2023-03-02 15:09:19 +0900

    done_part7

---
+++ pointer_practice/part7/sample.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    char c[] = "JOHN";
+    // c[0] = "J";
+    // c[1] = "o";
+    // c[2] = "h";
+    // c[3] = "n";
+    cout << strlen(c) << endl;
+    cout << sizeof(c) << endl;
+    return 0;
+}
